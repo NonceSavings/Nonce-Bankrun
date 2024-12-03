ac682a9c33616d64bfe5c1a636eb8722d7163598
Ofuzor chukwuemeke <uniben2018@gmail.com>
2024-12-03 23:38:43 +0100

    test

--- programs/nonce/src/constants.rs
+++ programs/nonce/src/constants.rs
@@ -5,3 +5,4 @@ pub const SOL_USD_FEED_ID: &str =
 pub const USDC_USD_FEED_ID: &str =
     "0xeaa020c61cc479712813461ce153894a96a6c00b21ed0cfc2798d1f9a9e9c94a";
 pub const MAXIMUM_AGE: u64 = 100; // allow price feed 100 sec old, to avoid stale price feed errors
+// pyth-solana-receiver-sdk = "0.3.2"
